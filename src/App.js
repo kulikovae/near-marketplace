8a791ca2c1a0dae4d3987329bcf39d5394665933
kulikovae <forkulikova@gmail.com>
2022-09-12 18:47:07 +0300

    Deploy version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useCallback, useState } from "react";
+import { Container, Nav } from "react-bootstrap";
+import { login, logout as destroy, accountBalance } from "./utils/near";
+import Wallet from "./components/Wallet";
+// import { Notification } from "./components/utils/Notifications";
+// import Products from "./components/marketplace/Products";
+import Cover from "./components/utils/Cover";
+import coverImg from "./assets/img/sandwich.jpg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Notification } from "./components/utils/Notifications";
+import Products from "./components/marketplace/Products";
+
+//..
+const App = function AppWrapper() {
+  const account = window.walletConnection.account();
+  const [balance, setBalance] = useState("0");
+  const getBalance = useCallback(async () => {
+    if (account.accountId) {
+      setBalance(await accountBalance());
+    }
+  });
+
+  useEffect(() => {
+    getBalance();
+  }, [getBalance]);
+//..
+
+//..
+return (
+  <>
+    { <Notification /> }
+    {account.accountId ? (
+      <Container fluid="md">
+        <Nav className="justify-content-end pt-3 pb-5">
+          <Nav.Item>
+            <Wallet
+              address={account.accountId}
+              amount={balance}
+              symbol="NEAR"
+              destroy={destroy}
+            />
+          </Nav.Item>
+        </Nav>
+        <main>{ <Products /> }</main>
+      </Container>
+    ) : (
+      <Cover name="Street Food" login={login} coverImg={coverImg} />
+    )}
+  </>
+);
+};
 
 export default App;
